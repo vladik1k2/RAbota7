8835bdb7e4cdf89c03d13ebb46592c2d68ebc165
vladik1k2 <116862129+vladik1k2@users.noreply.github.com>
2022-11-26 10:56:56 +0700

    Dalshe

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -5,11 +5,13 @@ namespace ConsoleApp1
     class Program
     {
         static void Main(string[] args)
-        {   
+        {   for(int i= 1;i<30; i++)
+            { 
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
         }
+    }
     }
 }
